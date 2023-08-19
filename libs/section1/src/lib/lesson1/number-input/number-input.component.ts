7212cc2769f0cc43cd91ceefb9c1704bfad6a856
Faizou <faizouaremou@gmail.com>
2023-08-19 19:45:58 +0200

    text and number

--- libs/section1/src/lib/lesson1/number-input/number-input.component.ts
+++ libs/section1/src/lib/lesson1/number-input/number-input.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Subject, startWith, takeUntil } from 'rxjs';
 
+const UPPER_NUMBER = 30;
+const LOWER_NUMBER = 5;
+const INITIAL_VALUE = 20;
 @Component({
   selector: 'forms-course-number-input',
   templateUrl: './number-input.component.html',
-  styleUrls: ['./number-input.component.css']
+  styleUrls: ['./number-input.component.css'],
 })
-export class NumberInputComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class NumberInputComponent implements OnInit, OnDestroy {
+  history: number[] = [];
+  control = new FormControl<number>(INITIAL_VALUE, { updateOn: 'blur' });
+  private _destroying$ = new Subject<void>();
+  constructor() {}
+  ngOnDestroy(): void {
+    this._destroying$.next();
   }
 
+  ngOnInit() {
+    this.control.valueChanges
+      .pipe(takeUntil(this._destroying$), startWith(this.control.value))
+      .subscribe((value) => {
+        if (value > UPPER_NUMBER) {
+          this.control.setValue(UPPER_NUMBER);
+        } else if (value < LOWER_NUMBER) {
+          this.control.setValue(LOWER_NUMBER);
+        }
+
+        this.history = [...this.history, value];
+      });
+  }
+  clearHistory() {
+    this.history = [];
+  }
 }
--- libs/section1/src/lib/lesson1/text-input/text-input.component.ts
+++ libs/section1/src/lib/lesson1/text-input/text-input.component.ts
@@ -1,20 +1,45 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Subject, startWith, takeUntil } from 'rxjs';
 
+const MY_FIRST_NAME = 'Faïzou';
+const MY_LAST_NAME = 'AREMOU';
 @Component({
   selector: 'forms-course-text-input',
   templateUrl: './text-input.component.html',
-  styleUrls: ['./text-input.component.css']
+  styleUrls: ['./text-input.component.css'],
 })
 export class TextInputComponent implements OnInit, OnDestroy {
+  history: string[] = [];
+  control = new FormControl<string>('');
+  private _destroying$ = new Subject<void>();
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.control.valueChanges
+      .pipe(takeUntil(this._destroying$), startWith(this.control.value))
+      .subscribe((value) => {
+        this.history = [value, ...this.history];
+      });
+  }
 
-  ngOnDestroy() {}
+  ngOnDestroy() {
+    this._destroying$.next();
+  }
 
-  setFormToMyName() {}
+  setFormToMyName(): void {
+    this.control.setValue(`${MY_FIRST_NAME} ${MY_LAST_NAME}`);
+  }
 
-  toggleFormEnabled() {}
+  toggleFormEnabled() {
+    if (this.control.disabled) {
+      this.control.enable({ emitEvent: false });
+    } else {
+      this.control.disable({ emitEvent: false });
+    }
+  }
 
-  clearHistory() {}
+  clearHistory() {
+    this.history = [];
+  }
 }
