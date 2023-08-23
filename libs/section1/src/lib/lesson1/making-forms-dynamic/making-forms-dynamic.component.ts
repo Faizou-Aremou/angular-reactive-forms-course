6ed8842f3cd95776c1625fefc8fcbd508ac7af61
Faïzou Aremou <faizouaremou@gmail.com>
2023-08-23 19:35:16 +0200

    finish formControl

--- libs/section1/src/lib/lesson1/making-forms-dynamic/making-forms-dynamic.component.ts
+++ libs/section1/src/lib/lesson1/making-forms-dynamic/making-forms-dynamic.component.ts
@@ -4,9 +4,17 @@ import { FormControl } from '@angular/forms';
 @Component({
   selector: 'forms-course-making-forms-dynamic',
   templateUrl: './making-forms-dynamic.component.html',
-  styleUrls: ['./making-forms-dynamic.component.css']
+  styleUrls: ['./making-forms-dynamic.component.css'],
 })
 export class MakingFormsDynamicComponent {
-  favoriteColor = new FormControl('Blue');
+  favoriteColor = new FormControl<string>('Blue');
   colors = ['Blue', 'Red', 'White', 'Orange', 'Purple', 'Yellow', 'Chartreuse'];
+  colorsControl = new FormControl<string>(this.colors.join(', '));
+  constructor() {
+    this.colorsControl.valueChanges.subscribe((value) => {
+      this.colors = value
+        .split(', ')
+        .filter((value) => value !== '');
+    });
+  }
 }
